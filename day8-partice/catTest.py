20613d32cd19da6990172396c2c48bff1fa3033e
nasirdiu <nasir.joypurhat@gmail.com>
2022-11-03 17:22:47 +0600

    paritce day8 inheritance polymorfijom

---
+++ day8-partice/catTest.py
@@ -0,0 +1,8 @@
+from cat import Cat
+tom = Cat("Tom", 3, 20)
+
+print ("Call move() method")
+tom.move()
+print ("\n")
+print ("Call showInfo() method")
+tom.showInfo()
---
+++ day8-partice/testVariablePlayer.py
@@ -0,0 +1,13 @@
+from player import Player
+
+player1 = Player("Tom", 20)
+player2 = Player("Jerry", 20)
+
+print ("Player.minAge = ", Player.minAge)
+print ("player1.minAge = ", player1.minAge)
+print ("player2.minAge = ", player2.minAge)
+
+Player.minAge = 19
+print ("Player.minAge = ", Player.minAge)
+print ("player1.minAge = ", player1.minAge)
+print ("player2.minAge = ", player2.minAge)
