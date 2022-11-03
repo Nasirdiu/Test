34e443f253d34f70ee8d4671406eca518a4aa485
nasirdiu <nasir.joypurhat@gmail.com>
2022-11-03 11:05:50 +0600

    paritce day8

---
+++ day8-partice/testPerson.py
@@ -0,0 +1,13 @@
+from person import Person
+#create a person
+mila = Person("Mila", 20, "Female")
+mila.showInfo()
+print(".................")
+#defunt person
+nasir = Person( "Nasir" )
+nasir.showInfo()
+#create name age defult gender
+print('...............')
+pallob = Person("Pallob", 25)
+pallob.showInfo()
+print('.............')
