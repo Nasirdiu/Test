1704b002769996f16b4482e44d3ec26543aab4b7
nasirdiu <nasir.joypurhat@gmail.com>
2022-11-07 22:44:27 +0600

    dict

---
+++ day9/dictionary.py
@@ -0,0 +1,36 @@
+'''
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict)
+'''
+'''
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict["model"])
+'''
+'''
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964,
+  "year": 2020,
+  "year":2022,
+}
+print(thisdict)
+print(len(thisdict))
+print(type(thisdict))
+'''
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+x = thisdict.keys()
+
+print(x)
